ddb7f1eb520141abe2b3e21ec3545075e849986d
SHEHAN22000 <shehanmsuv@gmail.com>
2022-12-01 11:41:35 +0200

    Shehan
    Code added

---
+++ MonitorApp/src/Monitor.java
@@ -0,0 +1,18 @@
+
+public class Monitor {
+
+public boolean isOn;
+	
+	public void start()
+	{
+		isOn = true;
+		
+	}
+	
+	public void shutdown()
+	{
+		isOn = false;
+	}
+	
+}
+
---
+++ MonitorApp/src/MonitorApp.java
@@ -0,0 +1,25 @@
+public class MonitorApp {
+
+	public static void main(String[] args) {
+	
+		Monitor monitor1 = new Monitor();
+		monitor1.start();
+		Monitor monitor2 = new Monitor();
+		
+		if(monitor1.isOn)
+		{
+		System.out.println("Monitor1 is On");
+		}
+		
+		if(monitor2.isOn)
+		{
+		System.out.println("Monitor2 is On");
+		}
+		
+		else {
+			System.out.println("Monitor2 is Off");
+		}
+		
+	}
+
+}
